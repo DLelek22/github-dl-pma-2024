71486d6db98f1e44f117af5981891a82167ebad0
DLelek22 <david.lelek@tul.cz>
2024-10-01 20:43:08 +0200

    aktualizace

--- MyApp02LinearLayout/app/src/main/java/com/example/myapp02linearlayout/MainActivity.kt
+++ MyApp02LinearLayout/app/src/main/java/com/example/myapp02linearlayout/MainActivity.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val editText = findViewById<EditText>(R.id.et_test)
+        val textView = findViewById<TextView>(R.id.tv_list)
+        val button = findViewById<Button>(R.id.btn_add)
+        val deleteButton = findViewById<Button>(R.id.btn_delete)
+
+        button.setOnClickListener {
+            val newText = editText.text.toString()
+            textView.append("a fa fa \n$newText")
+            editText.text.clear()
+
         }
+
     }
+
 }
