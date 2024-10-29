0f7efdac51d4ba988517be5974825717e756bc57
DLelek22 <david.lelek@tul.cz>
2024-10-29 11:52:49 +0100

    uhlazení

--- MyApp01LinearLayout/app/src/main/java/com/example/myapp01linearlayout/MainActivity.kt
+++ MyApp01LinearLayout/app/src/main/java/com/example/myapp01linearlayout/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapp01linearlayout
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,15 +14,7 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        */
 
 
         //Jednotlivé reference na EditText
