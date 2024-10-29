fefce4cc7e95f24996bca50ebc9d7ae85099238e
DLelek22 <david.lelek@tul.cz>
2024-10-29 12:46:45 +0100

    toast + snackbar

--- MyApp007ToastSnackbar/app/src/main/java/com/example/myapp007toastsnackbar/MainActivity.kt
+++ MyApp007ToastSnackbar/app/src/main/java/com/example/myapp007toastsnackbar/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapp007toastsnackbar
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
@@ -10,20 +11,31 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
+import android.widget.Toast
+import com.example.myapp007toastsnackbar.databinding.ActivityMainBinding
+import android.view.Gravity
+import android.graphics.Color
+import androidx.core.view.accessibility.AccessibilityEventCompat.setAction
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     private var job: Job? = null
     var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnstartstop = findViewById<Button>(R.id.btnStartStop)
-        val btnreset = findViewById<Button>(R.id.btn_reset)
-        val btnsplit = findViewById<Button>(R.id.btn_mezicas)
+        //val btnstartstop = findViewById<Button>(R.id.btnStartStop)
+        //val btnreset = findViewById<Button>(R.id.btn_reset)
+        //val btnsplit = findViewById<Button>(R.id.btn_mezicas)
 
-        btnstartstop.setOnClickListener {
+        binding.btnStartStop.setOnClickListener {
             if (job == null || job?.isCancelled == true) {
                 startCounter()
             } else {
@@ -31,13 +43,38 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        btnreset.setOnClickListener {
+        binding.btnReset.setOnClickListener {
             resetCounter()
             stopCounter()
+           // val toastReset = Toast.makeText(this@MainActivity, "TIMER has reset", Toast.LENGTH_LONG)
+
+            val customToastLayout = layoutInflater.inflate(R.layout.custom_layout,null)
+            val customToast = Toast(this)
+            customToast.view = customToastLayout
+            customToast.setGravity(Gravity.BOTTOM,0,0)
+            customToast.duration = Toast.LENGTH_SHORT
+            customToast.show()
+
+
+
         }
 
-        btnsplit.setOnClickListener {
+        binding.btnMezicas.setOnClickListener {
             SplitTime()
+        }
+
+        binding.btnSnackbar.setOnClickListener {
+
+                Snackbar.make(binding.root, "Já jsem SNACKBAR",Snackbar.LENGTH_SHORT)
+
+                .setDuration(7000)
+                .setBackgroundTint(Color.parseColor("#4D4D4D"))
+                .setTextColor(Color.WHITE)
+                .setActionTextColor(Color.WHITE)
+                .setAction("Zavřít") {
+                    Toast.makeText(this, "Zavírám SNACKBAR", Toast.LENGTH_SHORT).show()
+                }
+                .show()
         }
 
     }
@@ -89,4 +126,5 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
 }
